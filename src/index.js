bea33dff93aad99659293eaf09322567feb1ffd4
amreek07 <164839260+amreek07@users.noreply.github.com>
2025-09-15 12:03:23 +0530

    Utils code implemented

--- src/index.js
+++ src/index.js
@@ -5,5 +5,15 @@ dotenv.config({
 });
 
 import connectDB from "./db/db.js";
+import { app } from "./app.js";
 
-connectDB();
+connectDB()
+.then(()=>{
+  app.listen(process.env.PORT || 8000, ()=> {
+    console.log(`Server is running at port ${process.env.PORT || 8000}`);
+  })
+})
+.catch((err)=>{
+  console.log("Error in connecting to database", err);
+  process.exit(1);
+})
