adce9dd605595cd8a95f3cb0c88c01d8eae680b3
amreek07 <164839260+amreek07@users.noreply.github.com>
2025-09-15 13:24:27 +0530

    Models created for user and video

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -5,3 +5,20 @@ const asyncHandler = (requestHandler) => {
 }
 
 export { asyncHandler };
+
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }   we can write the asyncHandler like this also
