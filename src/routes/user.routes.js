8335d8e98ab8eab0077fc10044366bee74c12ec3
amreek07 <164839260+amreek07@users.noreply.github.com>
2025-09-18 10:50:07 +0530

    Add watch history route and refactor user routes for consistency

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,32 +1,61 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refresheAccessToken, registerUser, updateAccountDetails, updateAvatar, updateCoverImage } from "../controllers/user.controller.js";
+import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  loginUser,
+  logoutUser,
+  refresheAccessToken,
+  registerUser,
+  updateAccountDetails,
+  updateAvatar,
+  updateCoverImage,
+  getWatchHistory
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(
-    upload.fields([
-        { 
-            name: "avatar",
-            maxCount: 1
-        },
-        {
-            name: "coverImage",
-            maxCount: 1
-        }
-    ]),
-    registerUser
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
 );
 
 router.route("/login").post(loginUser);
 
 //secured routes
-router.route("/logout").post(verifyJWT, logoutUser);
-router.route("/refresh-token").post(refresheAccessToken); //here we can use verifyjwt middleware but we have already used all the security checks in the controllers so we can neglect it.
-router.route("/update-user-details").post(verifyJWT, updateAccountDetails);
-router.route("/update-user-avatar").post(verifyJWT, upload.single("avatar") ,updateAvatar);
-router.route("/update-user-cover-image").post(verifyJWT, upload.single("coverImage") ,updateCoverImage);
+router
+  .route("/logout")
+  .post(verifyJWT, logoutUser);
+router
+  .route("/refresh-token")
+  .post(refresheAccessToken); //here we can use verifyjwt middleware but we have already used all the security checks in the controllers so we can neglect it.
+router
+  .route("/change-password")
+  .post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser); 
+router
+  .route("/update-user-details")
+  .patch(verifyJWT, updateAccountDetails);
+router
+  .route("/update-user-avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateAvatar);
+router
+  .route("/update-user-cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateCoverImage);
 
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
+// New route for fetching user watch history
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
 
 export default router;
